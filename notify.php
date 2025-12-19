b7ae0dff59b4a900b84b26662cfd3dcf3968307c
ThePavlys <pavel.dousa9@gmail.com>
2025-12-19 13:58:24 +0100

    Update notify.php

--- notify.php
+++ notify.php
@@ -2,7 +2,7 @@
 $to = "pavel.dousa9@gmail.com";
 $subject = "Stránka byla otevřena";
 $message = "Stránka byla otevřena z IP: " . $_SERVER['REMOTE_ADDR'];
-$headers = "From: VstupenkaProAničku";
+$headers = "pavel.dousa9@gmail.com";
 
 mail($to, $subject, $message, $headers);
 ?>
