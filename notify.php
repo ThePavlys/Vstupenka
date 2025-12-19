0dfc36f85b2846b7b7b86500b360993c67c486b5
ThePavlys <pavel.dousa9@gmail.com>
2025-12-19 14:51:04 +0100

    Update and rename notify.php to index2.html

--- notify.php
+++
@@ -1,8 +0,0 @@
-<?php
-$to = "pavel.dousa9@gmail.com";
-$subject = "Stránka byla otevřena";
-$message = "Stránka byla otevřena z IP: " . $_SERVER['REMOTE_ADDR'];
-$headers = "pavel.dousa9@gmail.com";
-
-mail($to, $subject, $message, $headers);
-?>
